db4e63c3f937b96391dfe37a09c61553b051899f
Ahmad Khayyat <akhayyat@gmail.com>
2013-06-25 23:01:27 -0400

    Clean rendered HTML using mezzanine RICHTEXT_* settings
    After rendering markdown into HTML, HTML is cleaned as per the
    RICHTEXT_ALLOWED_TAGS, RICHTEXT_ALLOWED_ATTRIBUTES, and
    RICHTEXT_ALLOWED_STYLES mezzanine settings.
    
    Tags, attributes, and styles that are not explicitly allowed are
    stripped out.

--- mezzanine_pagedown/filters.py
+++ mezzanine_pagedown/filters.py
@@ -1,17 +1,26 @@
 from markdown import markdown
+from bleach import clean
+
+
+def _clean(html):
+    tags = settings.RICHTEXT_ALLOWED_TAGS
+    attrs = settings.RICHTEXT_ALLOWED_ATTRIBUTES
+    styles = settings.RICHTEXT_ALLOWED_STYLES
+    return clean(html, tags=tags, attributes=attrs, strip=True,
+                 strip_comments=False, styles=styles)
+
 
 def codehilite(content):
     """
     Renders content using markdown with the codehilite extension.
     """
-    return markdown(content, ['codehilite',])
+    return _clean(markdown(content, ['codehilite',]))
 
 
 def plain(content):
     """
     Renders content using markdown (no extensions).
-    Explicit is better than implicit....
     """
-    return markdown(content)
+    return _clean(markdown(content))
 
 
