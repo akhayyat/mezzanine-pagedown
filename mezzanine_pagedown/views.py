92476af7933adb9e12615d5cd254002dc2b907b8
Ahmad Khayyat <akhayyat@gmail.com>
2013-06-25 22:56:01 -0400

    Use RICHTEXT_FILTER to generate server-side previews

--- mezzanine_pagedown/views.py
+++ mezzanine_pagedown/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse
 from django.views.generic import View
 
-from markdown import markdown
+from mezzanine.conf import settings
+from mezzanine.utils.importing import import_dotted_path
+
+
+render = import_dotted_path(settings.RICHTEXT_FILTER)
+
 
 class MarkupPreview(View):
-    """Renders markdown content to HTML for preview purposes."""
+    """
+    Renders markdown content to HTML for preview purposes.
+    """
 
     http_method_names = ['post']
 
     def post(self, request, *args, **kwargs):
         text = self.request.POST.get('text', u"")
-        return HttpResponse(markdown(text), content_type='text/html')
+        return HttpResponse(render(text), content_type='text/html')
