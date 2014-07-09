59a569700aa4721653288197fb6f6c2a4d1741c7
Ahmad Khayyat <akhayyat@gmail.com>
2014-07-09 19:51:10 +0300

    Bump version to 0.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setup(
     name = "mezzanine-pagedown",
-    version = "0.6",
+    version = "0.7",
     description = "PageDown rich text widget for Mezzanine",
     long_description = readme.read(),
     author = "Ahmad Khayyat",
