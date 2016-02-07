5614297936417cb11254733a935ad726214ca85e
Robert Moggach <rob@moggach.com>
2016-02-07 17:59:21 -0500

    updated for Django > 1.8

--- mezzanine_pagedown/widgets.py
+++ mezzanine_pagedown/widgets.py
@@ -3,7 +3,11 @@ from django.utils.safestring import mark_safe
 from django.template.loader import render_to_string
 from django.utils.html import conditional_escape
 from django.utils.encoding import force_text
-from django.forms.util import flatatt
+from django import VERSION as DJANGO_VERSION
+if DJANGO_VERSION < (1, 8):
+    from django.forms.util import flatatt
+else:
+    from django.forms.utils import flatatt
 
 from mezzanine.conf import settings
 
