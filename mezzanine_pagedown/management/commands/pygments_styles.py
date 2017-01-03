886c83a13600075c6e4c19623dc1cafd76809140
maneesh yadav <maneeshkyadav@gmail.com>
2017-01-03 23:27:55 +0000

    pygments_styles.py edited online with Bitbucket

--- mezzanine_pagedown/management/commands/pygments_styles.py
+++ mezzanine_pagedown/management/commands/pygments_styles.py
@@ -13,16 +13,15 @@ else:
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('scheme')
+        parser.add_argument('--scheme', dest='scheme_name', type=str, help="""Available color schemes:
+""" + '\n'.join(["  %s" % name for name in get_all_styles()])))
+
         parser.add_argument('--all', dest='all_styles', action='store_true')
 
     def handle(self, *args, **options):
         if not PYGMENTS:
             raise CommandError('Unable to load pygments. '
                                'Please install pygments to use this command.')
-
-        print args
-        print options
 
         if options['all_styles']:
             for scheme in get_all_styles():
@@ -32,13 +31,8 @@ class Command(BaseCommand):
             sys.exit(0)
 
 
-        if len(args) == 0:
-            print("""
-Usage: ./manage.py pygments_styles <scheme_name>
-Available color schemes:
-""" + '\n'.join(["  %s" % name for name in get_all_styles()]))
-        else:
-            scheme=args[0]
+        if options['scheme_name']:
+            scheme=options['scheme_name']
             try:
                 assert(scheme in list(get_all_styles()))
             except AssertionError:
